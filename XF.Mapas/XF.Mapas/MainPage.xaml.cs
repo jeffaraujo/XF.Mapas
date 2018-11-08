1e22e17c7f101eef27d15c46226e7231b7cf9b1f
Logon Aluno <logonrmlocal@fiap.com.br>
2018-11-07 22:57:41 -0200

    Add project files.

---
+++ XF.Mapas/XF.Mapas/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF.Mapas
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void OnMapa_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new View.MapaView());
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new View.ControleView());
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new View.LocalizacaoView());
+        }
+
+        private void OnMarcacao_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new View.MarcarView());
+        }
+    }
+}
